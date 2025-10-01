7365db5ed6fc51d95f634894760187070bc1c032
coletm <coletmorrison18@gmail.com>
2025-10-01 14:20:07 -0400

    added json.simple dependency

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,11 @@
 module com.escape {
     requires javafx.controls;
     requires javafx.fxml;
+    requires json.simple;
 
     opens com.escape to javafx.fxml;
     exports com.escape;
+
+    opens com.model to javafx.fxml;
+    exports com.model;
 }
