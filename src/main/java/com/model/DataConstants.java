a9e1f397e1aebfd74dccc6ad01541e5b8040a34b
thesubspacecommando <efirdevan@gmail.com>
2025-10-02 14:21:38 -0400

    Added Data constants + moved things around

---
+++ src/main/java/com/model/DataConstants.java
@@ -0,0 +1,13 @@
+package com.model;
+
+public abstract class DataConstants {
+    protected static final String USER_FILE_NAME = "src/main/java/com/json/users.json";
+    protected static final String USER_ID = "userId";
+    protected static final String FIRST_NAME = "firstName";
+    protected static final String LAST_NAME = "lastName";
+    protected static final String EMAIL = "email";
+    protected static final String PASSWORD = "password";
+    protected static final String SKILL_LEVEL = "skillLevel";
+    protected static final String CARACTER = "character";
+    protected static final String RECORD = "personalRecord";
+}
