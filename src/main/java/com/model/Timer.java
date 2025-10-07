0e7df89929a26d153e03c7892e0fc445b4ab981f
kls36 <kls36.sc.edu>
2025-10-07 14:50:49 -0400

    working on #1

--- src/main/java/com/model/Timer.java
+++ src/main/java/com/model/Timer.java
@@ -1,5 +1,35 @@
 package com.model;
 
 public class Timer {
-    
+    private double timeRemaining;
+    private double initialTime;
+    private boolean isRunning;
+
+    public Timer() {
+        this.timeRemaining = 30.00;
+        this.initialTime = 0;
+        this.isRunning = isRunning;
+    }
+    public Timer(double timeRemaining, boolean isRunning){
+        this.timeRemaining = timeRemaining;
+        this.initialTime = initialTime;
+    }
+    public void start(){
+        isRunning = true;
+    }
+    public void pause() {
+        isRunning = false;
+    }
+    public void resume() {
+        isRunning = true;
+    }
+    public void addTime(int seconds){
+        timeRemaining += seconds;
+    }
+    public void subtractTime(int seconds){
+        timeRemaining -= seconds;
+    }
+    public boolean isExpired(){
+        return timeRemaining <= 0;
+    }
 }
