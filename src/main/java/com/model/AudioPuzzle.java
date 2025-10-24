e1a10a83d1d8b648b5c4a954d78bbdbec0a300da
Njhippchen <hippchn@email.sc.edu>
2025-10-24 16:37:02 -0400

    JavaDoc #45

--- src/main/java/com/model/AudioPuzzle.java
+++ src/main/java/com/model/AudioPuzzle.java
@@ -13,7 +13,17 @@ public class AudioPuzzle extends Puzzle
 
     private String audioContent;
     private int audioSolution;
-
+    /*
+     * Constructor for AudioPuzzle
+     * @param puzzleID The unique identifier for the puzzle
+     * @param difficulty The difficulty level of the puzzle
+     * @param attempts The number of attempts allowed to solve the puzzle
+     * @param clue The clue associated with the puzzle
+     * @param hints A list of hints available for the puzzle
+     * @param isSequential A boolean indicating if hints must be used in order
+     * @param audioContent The audio content of the puzzle
+     * @param audioSolution The solution to the audio puzzle
+     */
     public AudioPuzzle(UUID puzzleID, Difficulty difficulty, int attempts, Clue clue,
                        ArrayList<Hint> hints, boolean isSequential, String audioContent, int audioSolution)
     {
@@ -23,14 +33,27 @@ public class AudioPuzzle extends Puzzle
         this.type = Type.Audio;
     }
 
+    /*
+     * Gets and returns the audio content
+     * @return The audio content of the puzzle
+     */
     public Object getContent() {
         return audioContent;
     }
 
+    /*
+     * Gets and returns the solution to the audio puzzle
+     * @return The solution to the audio puzzle
+     */
     public Object getSolution() {
         return audioSolution;
     }
 
+    /*
+     * Checks if the provided answer is correct
+     * @param answer, The users answer 
+     * @return True if the answer is correct, false otherwise
+     */
     public boolean solve(Object answer)
     {
         if (answer instanceof Integer)
