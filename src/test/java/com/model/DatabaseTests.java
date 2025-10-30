7a965c5edfa556a5dbd2693e84580a76871cc1ef
thesubspacecommando <efirdevan@gmail.com>
2025-10-30 13:50:38 -0400

    Added jupiter dependancy and new json files to test folder

---
+++ src/test/java/com/model/DatabaseTests.java
@@ -0,0 +1,13 @@
+package com.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+
+public class DatabaseTests {
+    
+
+    @BeforeEach void Test() {
+
+    }
+}
