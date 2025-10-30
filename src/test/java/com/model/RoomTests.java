77b645ef854e40ca534a6ce655c2f95c30fd2489
Njhippchen <hippchn@email.sc.edu>
2025-10-30 14:25:50 -0400

    Created testing #70 and #72

---
+++ src/test/java/com/model/RoomTests.java
@@ -0,0 +1,12 @@
+package com.model;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.jupiter.api.BeforeEach;
+
+public class RoomTests {
+    
+    @BeforeEach void Test() {
+
+    }
+}
