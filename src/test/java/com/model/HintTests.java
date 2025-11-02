012114b83a486e5ccc3109c2bc74a7605d964097
thesubspacecommando <efirdevan@gmail.com>
2025-11-02 12:17:44 -0500

    Removed some unneeded setup/teardown

--- src/test/java/com/model/HintTests.java
+++ src/test/java/com/model/HintTests.java
@@ -1,33 +1,16 @@
 package com.model;
 
-import java.util.ArrayList;
 import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+
 
 public class HintTests {
-    private UserList userList = UserList.getInstance();
-    private ArrayList<User> users = userList.getUsers();
 
-    @BeforeEach
-    public void setup() {
-        users.clear();
-        userList.addUser("James", "Adams", "jadams@gmail.com", "Puzzler", "password");
-        userList.addUser("Markus", "Franks", "Marky@gmail.com", "MarkyMan", "M999222");
-        userList.addUser("Dianna", "Planter", "DIP@outlook.com", "DIPlanter", "Flimbly123");
-        userList.saveUsers();
-    }
 
-    @AfterEach
-    public void tearDown() {
-        UserList.getInstance().getUsers().clear();
-        DataWriter.saveUsers();
-    }
-    
+
     @Test
     public void testText() {
         Hint h = new Hint(UUID.randomUUID(),"test",false,null,HintLevel.Direct);
