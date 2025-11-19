3377e30d43a2d87c921649e7f8f2dd08b79474e5
thesubspacecommando <efirdevan@gmail.com>
2025-11-18 22:28:44 -0500

    Added functionality to Menu Buttons

---
+++ src/main/java/com/escape/MenuController.java
@@ -0,0 +1,22 @@
+package com.escape;
+
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+
+public class MenuController {
+    @FXML
+    public void switchToNew() throws IOException {
+        App.setRoot("NewGame");
+    }
+
+    @FXML
+    public void switchToContinue() throws IOException {
+        App.setRoot("Continue");
+    }
+
+    @FXML
+    public void switchToSettings() throws IOException {
+        App.setRoot("Settings");
+    }
+}
