362d19a7a592551775c59d9f97d137a9d45d6626
Njhippchen <hippchn@email.sc.edu>
2025-11-20 14:26:24 -0500

    worked on library

--- src/main/java/com/escape/LibraryController.java
+++ src/main/java/com/escape/LibraryController.java
@@ -1,5 +1,17 @@
 package com.escape;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class LibraryController {
+
+    @FXML
+    private Button pauseLibrary;
+
+    @FXML
+    void goToPause(ActionEvent event) {
+
+    }
     
 }
