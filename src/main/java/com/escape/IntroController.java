462979ce1df3c90913dfefc0305bc01ae21790ea
coletm <coletmorrison18@gmail.com>
2025-11-20 14:13:21 -0500

    added intro screen + controller skeleton

---
+++ src/main/java/com/escape/IntroController.java
@@ -0,0 +1,16 @@
+package com.escape;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class IntroController {
+
+    @FXML
+    private Button enterButton;
+
+    @FXML
+    void switchToHallway() {
+        // App.setRoot("hallway");
+    }
+
+}
