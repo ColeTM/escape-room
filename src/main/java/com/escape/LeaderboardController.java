64da4efc887e982ef3786f60b31e7cd5e7443787
Njhippchen <hippchn@email.sc.edu>
2025-11-20 13:58:49 -0500

    worked on #151

--- src/main/java/com/escape/LeaderboardController.java
+++ src/main/java/com/escape/LeaderboardController.java
@@ -1,12 +1,21 @@
 package com.escape;
 
 import java.io.IOException;
-
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import com.model.EscapeRoom;
+
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
 
 public class LeaderboardController {
+
+    @FXML
+    private GridPane grid_leaderboard;
+
+    @FXML
+    private Button returnFromLeaderboard;
 
     @FXML
     private void switchToLeaderboard() throws IOException {
@@ -14,11 +23,8 @@ public class LeaderboardController {
     }
 
     @FXML
-    private Button returnFromLeaderboard;
-
-    @FXML
-    void switchToMain(ActionEvent event) {
-
+    void switchToMain() throws IOException {
+       App.setRoot("main_menu");
     }
     
 }
