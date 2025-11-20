49a53200ca2fd69ff91f93f1cc32a2847a61a33e
Njhippchen <hippchn@email.sc.edu>
2025-11-20 13:21:17 -0500

    Worked on #151

--- src/main/java/com/escape/LeaderboardController.java
+++ src/main/java/com/escape/LeaderboardController.java
@@ -1,13 +1,24 @@
 package com.escape;
 
 import java.io.IOException;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class LeaderboardController {
 
     @FXML
     private void switchToLeaderboard() throws IOException {
         App.setRoot("leaderboard");
+    }
+
+    @FXML
+    private Button returnFromLeaderboard;
+
+    @FXML
+    void switchToMain(ActionEvent event) {
+
     }
     
 }
