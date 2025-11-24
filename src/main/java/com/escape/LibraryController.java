f37ac1a735a201cda410dad1a7b1080e687b6a9d
Njhippchen <hippchn@email.sc.edu>
2025-11-24 17:22:14 -0500

    worked on #161 and added hint icon

--- src/main/java/com/escape/LibraryController.java
+++ src/main/java/com/escape/LibraryController.java
@@ -7,7 +7,31 @@ import javafx.scene.control.Button;
 public class LibraryController {
 
     @FXML
+    private Button backLibrary;
+
+    @FXML
+    private Button blackBook;
+
+    @FXML
+    private Button blueBook;
+
+    @FXML
+    private Button greenBook;
+
+    @FXML
+    private Button hintLibrary;
+
+    @FXML
+    private Button inventoryLibrary;
+
+    @FXML
     private Button pauseLibrary;
+
+    @FXML
+    private Button puzzleLibrary;
+
+    @FXML
+    private Button redBook;
 
     @FXML
     void goToPause(ActionEvent event) {
