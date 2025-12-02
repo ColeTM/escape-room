63309fe255343f499fec33d994fce26f874d55c9
Njhippchen <hippchn@email.sc.edu>
2025-12-02 15:42:50 -0500

    worked on #162

---
+++ src/main/java/com/escape/PauseController.java
@@ -0,0 +1,34 @@
+package com.escape;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class PauseController {
+
+    
+    @FXML
+    private Button resumeButton;
+
+    @FXML
+    private Button saveandquit;
+
+    @FXML
+    private Button settingssButton;
+
+    @FXML
+    void goToSettings(ActionEvent event) {
+
+    }
+
+    @FXML
+    void resumeGame(ActionEvent event) {
+
+    }
+
+    @FXML
+    void saveThenQuit(ActionEvent event) {
+
+    }
+    
+}
