2f117030fb58c1ed095486dde72435d1006c0ae4
Cole <coletmorrison18@gmail.com>
2025-12-02 23:36:24 -0500

    added some functionality to pause menu

--- src/main/java/com/escape/PauseController.java
+++ src/main/java/com/escape/PauseController.java
@@ -1,6 +1,10 @@
 package com.escape;
 
-import javafx.event.ActionEvent;
+import java.io.IOException;
+import java.util.UUID;
+
+import com.model.EscapeRoom;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 
@@ -9,26 +13,40 @@ public class PauseController {
     
     @FXML
     private Button resumeButton;
-
     @FXML
     private Button saveandquit;
+    @FXML
+    private Button settingsButton;
 
     @FXML
-    private Button settingssButton;
+    public void initialize() {
+        // pause timer
+    }
 
     @FXML
-    void goToSettings(ActionEvent event) {
+    void goToSettings() throws IOException {
+        App.setRoot("Settings");
+    }
+
+    @FXML
+    void resumeGame() throws IOException{
+        if (EscapeRoom.getInstance().getCurrentCharacter().getCurrentRoom().equals(UUID.fromString("26767fe2-e8b1-47c4-b4eb-5f9aec77fb85")))
+            App.setRoot("main_hallway");
+        else if (EscapeRoom.getInstance().getCurrentCharacter().getCurrentRoom().equals(UUID.fromString("9aae693f-83a4-427e-9822-b150f44ba171")))
+            App.setRoot("library");
+        else if (EscapeRoom.getInstance().getCurrentCharacter().getCurrentRoom().equals(UUID.fromString("a91b9a37-d41d-4d93-b553-895ffd04723a")))
+            App.setRoot("box_room");
+        else if (EscapeRoom.getInstance().getCurrentCharacter().getCurrentRoom().equals(UUID.fromString("1030c684-0ceb-4f2b-a71e-f5846697d8d7")))
+            App.setRoot("final_puzzle");
+
+        // resume timer
 
     }
 
     @FXML
-    void resumeGame(ActionEvent event) {
-
-    }
-
-    @FXML
-    void saveThenQuit(ActionEvent event) {
-
+    void saveThenQuit() throws IOException {
+        EscapeRoom.getInstance().saveCurrentGame();
+        App.setRoot("Main_menu");
     }
     
 }
