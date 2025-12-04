8271876b7ef5e502d258c5525959f7d0423e3805
thesubspacecommando <efirdevan@gmail.com>
2025-12-03 22:28:53 -0500

    Hallway Controller added + update xml

---
+++ src/main/java/com/escape/HallwayController.java
@@ -0,0 +1,27 @@
+package com.escape;
+
+import java.lang.classfile.Label;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class HallwayController {
+    
+    @FXML
+    private Button puzzleLight;
+
+    @FXML
+    private Button puzzleLibrary;
+
+    @FXML
+    private Button puzzleBox;
+
+    @FXML
+    private Button invintory;
+
+    @FXML
+    private Button pause;
+
+    @FXML
+    private Label lockedText;
+}
