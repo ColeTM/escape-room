d7300e06e2dec74a6559faf441760f7bcd06492b
Cole <coletmorrison18@gmail.com>
2025-12-04 19:30:15 -0500

    added item screens

--- src/main/java/com/escape/Key1Controller.java
+++ src/main/java/com/escape/Key1Controller.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FlashlightController {
+public class Key1Controller {
 
     @FXML
     private Button backButton;
--- src/main/java/com/escape/Key2Controller.java
+++ src/main/java/com/escape/Key2Controller.java
@@ -4,7 +4,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-public class FlashlightController {
+public class Key2Controller {
 
     @FXML
     private Button backButton;
